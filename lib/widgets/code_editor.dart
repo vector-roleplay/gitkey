bc7f65c6e0603c9be1f5aefba114af5dee698a8b
死苏 <779908358@qq.com>
2025-12-29 14:28:11 +0800

    Update lib/widgets/code_editor.dart via AI Code Sync

--- lib/widgets/code_editor.dart
+++ lib/widgets/code_editor.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class CodeEditor extends StatelessWidget {
+class CodeEditor extends StatefulWidget {
   final TextEditingController controller;
   final VoidCallback? onChanged;
   final String language;
@@ -15,6 +15,37 @@ class CodeEditor extends StatelessWidget {
   });
 
   @override
+  State<CodeEditor> createState() => _CodeEditorState();
+}
+
+class _CodeEditorState extends State<CodeEditor> {
+  final ScrollController _scrollController = ScrollController();
+  int _lineCount = 1;
+
+  @override
+  void initState() {
+    super.initState();
+    _updateLineCount();
+    widget.controller.addListener(_updateLineCount);
+  }
+
+  void _updateLineCount() {
+    final count = '\n'.allMatches(widget.controller.text).length + 1;
+    if (count != _lineCount) {
+      setState(() {
+        _lineCount = count;
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    widget.controller.removeListener(_updateLineCount);
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final isDark = Theme.of(context).brightness == Brightness.dark;
     
@@ -23,55 +54,56 @@ class CodeEditor extends StatelessWidget {
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          // 行号
+          // 行号列
           Container(
-            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-            color: isDark ? const Color(0xFF252526) : const Color(0xFFF0F0F0),
-            child: ValueListenableBuilder<TextEditingValue>(
-              valueListenable: controller,
-              builder: (context, value, child) {
-                final lineCount = '\n'.allMatches(value.text).length + 1;
-                return Column(
+            width: 48,
+            color: isDark ? const Color(0xFF252526) : const Color(0xFFE8E8E8),
+            child: SingleChildScrollView(
+              controller: _scrollController,
+              physics: const NeverScrollableScrollPhysics(),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 8),
+                child: Column(
                   crossAxisAlignment: CrossAxisAlignment.end,
                   children: List.generate(
-                    lineCount,
-                    (index) => Text(
-                      '${index + 1}',
-                      style: TextStyle(
-                        fontFamily: 'monospace',
-                        fontSize: 14,
-                        height: 1.5,
-                        color: Colors.grey[500],
+                    _lineCount,
+                    (i) => SizedBox(
+                      height: 24,
+                      child: Text(
+                        '${i + 1}',
+                        style: TextStyle(
+                          fontFamily: 'monospace',
+                          fontSize: 14,
+                          height: 1.7,
+                          color: Colors.grey[500],
+                        ),
                       ),
                     ),
                   ),
-                );
-              },
+                ),
+              ),
             ),
           ),
           
-          // 代码区域
+          // 代码编辑区
           Expanded(
             child: SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: IntrinsicWidth(
-                child: TextField(
-                  controller: controller,
-                  onChanged: (_) => onChanged?.call(),
-                  readOnly: readOnly,
-                  maxLines: null,
-                  keyboardType: TextInputType.multiline,
-                  style: TextStyle(
-                    fontFamily: 'monospace',
-                    fontSize: 14,
-                    height: 1.5,
-                    color: isDark ? Colors.white : Colors.black87,
-                  ),
-                  decoration: const InputDecoration(
-                    contentPadding: EdgeInsets.all(8),
-                    border: InputBorder.none,
-                    isDense: true,
-                  ),
+              child: TextField(
+                controller: widget.controller,
+                onChanged: (_) => widget.onChanged?.call(),
+                readOnly: widget.readOnly,
+                maxLines: null,
+                keyboardType: TextInputType.multiline,
+                style: TextStyle(
+                  fontFamily: 'monospace',
+                  fontSize: 14,
+                  height: 1.7,
+                  color: isDark ? Colors.white : Colors.black87,
+                ),
+                decoration: const InputDecoration(
+                  contentPadding: EdgeInsets.all(12),
+                  border: InputBorder.none,
+                  isDense: true,
                 ),
               ),
             ),
